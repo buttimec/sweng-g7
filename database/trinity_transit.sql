3d27de9dea3e52bcf8f97f3cab405cac2831d2c2
Aranq <aranq13@gmail.com>
2025-03-16 15:00:59 +0000

    Update database with necessary tables

--- database/trinity_transit.sql
+++ database/trinity_transit.sql
@@ -13,19 +13,53 @@ USE trinity_transit;
 DROP TABLE IF EXISTS buses;
 
 -- Create a table which contains buses
-CREATE TABLE buses (
+CREATE TABLE TransportProviders (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR (50)
+    name VARCHAR (50),
+    vehicleType VARCHAR (50)
 );
 
 
-INSERT INTO buses (name)
-VALUES 
-    ('1A'),
-    ('2C'),
-    ('41'),
-    ('500X'),
-    ('197'),
-    ('436'),
-    ('3'),
-    ('7A');
+CREATE TABLE BusStop (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR (50),
+    location VARCHAR (50),
+);
+
+CREATE TABLE Routes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR (50),
+    startLocation INT,
+    FOREIGN KEY startLocation REFERENCES BusStop(id)
+)
+
+CREATE TABLE RoutesStops (
+    route_id INT,
+    stop_id INT,
+    FOREIGN KEY route_id REFERENCES Routes(id)
+    FOREIGN KEY stop_id REFERENCES BusStop(id)
+)
+
+
+CREATE TABLE PersonalDetails (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR (50),
+    email VARCHAR (50),
+    home VARCHAR (200),
+    work VARCHAR (200),
+);
+
+CREATE TABLE RoutesFavourites (
+    route_id INT,
+    user_id INT,
+    FOREIGN KEY route_id REFERENCES Routes(id)
+    FOREIGN KEY user_id REFERENCES PersonalDetails(id)
+)
+
+CREATE TABLE ProvidersFavourites (
+    provider_id INT,
+    user_id INT,
+    FOREIGN KEY provider_id REFERENCES TransportProviders(id)
+    FOREIGN KEY user_id REFERENCES PersonalDetails(id)
+)
+
