9e9720a14a7fdf3de44021d586771046d1ce791c
kogandg <kogan.dg@gmail.com>
2025-03-18 17:38:26 +0000

    Exploring changes

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
-export const BACKEND_URL = 'https://ca71-134-226-213-136.ngrok-free.app' 
+export const BACKEND_URL = 'https://d975-185-134-146-163.ngrok-free.app' 
 
 // enter your backend url (either localhost or ngrok) 
 // Note: ngrok forwarding address changes everytime when starting a ngrok server - free account version
