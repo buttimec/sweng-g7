81041d3a651641d49dca8f77698ddd4bf2f5a2b8
ShinoaK <oscar217213400@outlook.com>
2025-04-09 10:54:53 +0100

    dublin bus fix

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
-export const BACKEND_URL = 'https://8577-134-226-213-136.ngrok-free.app' 
+export const BACKEND_URL = 'https://81b1-134-226-213-136.ngrok-free.app' 
 
 // enter your backend url (either localhost or ngrok) 
 // Note: ngrok forwarding address changes everytime when starting a ngrok server - free account version
